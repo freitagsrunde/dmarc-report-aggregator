c364ba19ce4cd58f023f25c7fd8feb1e170694ee
Maximilian Haye <maxh@freitagsrunde.org>
2023-11-21 20:20:55 +0100

    build: add docker-build.sh script

---
+++ docker-build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -o nounset -o errexit
+
+readonly VERSION="1.2.0"
+readonly IMAGE_REPO="ghcr.io/freitagsrunde/dmarc-report-aggregator"
+
+PUSH=false
+while getopts ":p" OPT; do
+  case "$OPT" in
+  p)
+    PUSH=true
+    ;;
+  *)
+    echo "usage: $0 [-p]" >&2
+    exit 1
+  esac
+done
+
+docker build "$(dirname "$0")" -t "$IMAGE_REPO:$VERSION" -t "$IMAGE_REPO:latest"
+
+if $PUSH; then
+  docker push "$IMAGE_REPO:$VERSION"
+  docker push "$IMAGE_REPO:latest"
+else
+  echo "Built $VERSION locally. Use -p to push." >&2
+fi
